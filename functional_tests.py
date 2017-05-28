b436bb5960bdf6994d278bf0adcdd62af5702b32
Stephen Langestraat <stephen.langestraat@gmail.com>
2017-05-28 11:50:36 +1000

    FT now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_open_home_page(self):
+        # Company A has heard about a cool new company called aialytics. She goes to
+        # check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention aialytics
+        self.assertIn('aialytics', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
